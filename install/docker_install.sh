8e02d3b3a5e9393919eb37b0d00c6cd2345a2cf9
Dostonbek Toirov <toirovd@berea.edu>
2019-06-13 15:16:02 -0400

    Changed offline install tasks

---
+++ install/docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce-18.09.1 docker-ce-cli-18.09.1 containerd.io
+sudo systemctl start docker
+sudo docker run hello-world
