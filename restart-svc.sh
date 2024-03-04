740206de286e74fa7c6840f7aec7e3b8f4db69ef
Alberto Bellotti <abellott@redhat.com>
2024-03-04 01:15:42 -0500

    Added service name check to restart-svc

--- restart-svc.sh
+++ restart-svc.sh
@@ -3,7 +3,11 @@ if [ -z "${SVC}" ]; then
   echo "Must specify which service to restart."
   exit 1
 fi
-echo "Stopping service discovery-${SVC} ..."
-systemctl --user stop discovery-${SVC}
-echo "Starting service discovery-${SVC} ..."
-systemctl --user start discovery-${SVC}
+if [ ! -f "discovery-${SVC}.container" ]; then
+  echo "Invalid Discovery service ${SVC} specified."
+  exit 1
+fi
+echo "Stopping service discovery-${SVC}.service ..."
+systemctl --user stop discovery-${SVC}.service
+echo "Starting service discovery-${SVC}.service ..."
+systemctl --user start discovery-${SVC}.service
