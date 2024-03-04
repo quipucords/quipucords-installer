c4ea4af824075bf237a3eeb0f475e237fdcf1231
Alberto Bellotti <abellott@redhat.com>
2024-03-04 14:12:44 -0500

    - Updated scale-workers to scale up or down workers - removed the start-worker and stop-worker helper scripts

--- scripts/scale-workers.sh
+++ scripts/scale-workers.sh
@@ -5,9 +5,31 @@
 #
 BASEDIR=$(dirname $0)
 source ${BASEDIR}/../config/env-server.env
-for WORKER in $(seq 1 ${QPC_MIN_CELERY_WORKERS})
+NUM_WORKERS="${QPC_MIN_CELERY_WORKERS}"
+if [ -n "${1}" ]; then
+  NUM_WORKERS="${1}"
+fi
+if [[ ${NUM_WORKERS} -gt ${QPC_MAX_CELERY_WORKERS} ]]; then
+  echo "Failed to scale the Discovery workers, maximum number of instances is ${QPC_MAX_CELERY_WORKERS}"
+  exit 1
+fi
+
+echo "Scaling Discovery Celery Workers discovery-celery-worker-@.service ..."
+echo -n "Starting celery workers: "
+for WORKER in $(seq 1 ${NUM_WORKERS})
 do
-  echo "Starting celery worker discovery-celery-worker-@${WORKER}.service ..."
+  echo -n "${WORKER} "
   systemctl --user start "discovery-celery-worker-@${WORKER}.service"
 done
+echo
+
+echo -n "Stopping celery workers: "
+for WORKER in $(seq ${NUM_WORKERS} ${QPC_MAX_CELERY_WORKERS})
+do
+  if [[ ${WORKER} -gt ${NUM_WORKERS} ]]; then
+    echo -n "${WORKER} "
+    systemctl --user stop "discovery-celery-worker-@${WORKER}.service"
+  fi
+done
+echo
 exit 0
