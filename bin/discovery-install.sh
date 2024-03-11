0642de4043d75028e965eb3142f6ed4a7ad8e5a2
Alberto Bellotti <abellott@redhat.com>
2024-03-11 18:15:14 -0400

    feature: Adding support for celery worker autoscaler.
    - Removed the multiple server instances of the celery workers
      for the Quadlet deployment.
    - Replaced with the single discovery-celery-worker.service that
      also gets started by the discovery-install script.
    - Updated the discovery-install script to restart the
      discovery services as we want the newly deployed ones.
    - Added the environment file env for the celery-worker.
    - Add environment variables QPC_CELERY_WORKER_MIN_CONCURRENCY
      and QPC_CELERY_WORKER_MAX_CONCURRENCY which are now honored
      by the Discovery server and enables the auto-scaler when
      then celery-worker is started via the make.

--- bin/discovery-install.sh
+++ bin/discovery-install.sh
@@ -16,19 +16,29 @@ if [ $? -ne 0 ]; then
   echo "Must create the discovery-django-secret-key secret."
   exit 1
 fi
+if [ ! -d ${HOME}/discovery ]; then
+  echo "Missing the discovery directory in ${HOME}."
+  exit 1
+fi
 systemctl --user reset-failed
 echo "Copying Discovery container and network files to the user systemd configuration ..."
 mkdir -p $HOME/.config/containers/systemd/
+cd ${HOME}/discovery
 cp config/*.network config/*.container $HOME/.config/containers/systemd/
 echo "Generate the systemd Discovery services ..."
 systemctl --user daemon-reload
 echo "Generated Services:"
 ls -l ${XDG_RUNTIME_DIR}/systemd/generator/discovery-*.service
-echo "Starting the Discovery services ..."
+echo "Re-starting the Discovery services ..."
 systemctl --user reset-failed
-systemctl --user start discovery-db
-systemctl --user start discovery-redis
-systemctl --user start discovery-server
+echo "  discovery-db ..."
+systemctl --user restart discovery-db
+echo "  discovery-redis ..."
+systemctl --user restart discovery-redis
+echo "  discovery-server ..."
+systemctl --user restart discovery-server
+echo "  discovery-celery-worker ..."
+systemctl --user restart discovery-celery-worker
 echo "Discovery Services:"
 systemctl --user list-units 'discovery-*'
 
